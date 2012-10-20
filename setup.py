115a03d046187d830bcda4ce3a5c7565b0f0b3c9
Randy Barlow <rbarlow@redhat.com>
2012-10-20 18:05:29 -0400

    Added a setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ari_backup',
+    version='0.9.0',
+    license='ARI',
+    packages=find_packages(),
+    author='American Research Institute',
+)
