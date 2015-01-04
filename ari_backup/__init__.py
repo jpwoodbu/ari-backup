7e64907b8c4a556122f858c61c97b9ad20a1ab09
Jonathan Woodbury <jpwoodbu@mybox.org>
2015-01-03 23:19:20 -0500

    Fixed bad last commit. Accidentally left in a conflict delimter.

--- ari_backup/__init__.py
+++ ari_backup/__init__.py
@@ -3,7 +3,6 @@ from rdiff_backup_wrapper import RdiffBackup
 from zfs import ZFSLVMBackup
 
 
-<<<<<<< HEAD
 # Continue to provide old names for backward compatiblity.
 ARIBackup = RdiffBackup
 LVMBackup = RdiffLVMBackup
